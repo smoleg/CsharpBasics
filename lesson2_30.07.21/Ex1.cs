372c8798948c01aadfd6c346649c9280a0161d06
Egor Smolyakov <smolyakov.en@gmail.com>
2021-07-30 22:54:17 +0300

    lesson2_300721

---
+++ lesson2_30.07.21/Ex1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2_30._07._21
+{
+    class Ex1
+    {
+        public static void ex1()
+        {
+            Console.WriteLine(Console.LargestWindowHeight);
+            Console.WriteLine(Console.LargestWindowWidth);
+
+            Console.WindowWidth = Console.LargestWindowWidth - 10;
+            Console.WindowHeight = Console.LargestWindowHeight - 10;
+
+            int ax = 60, ay = 1;
+            int bx = 2, by = 40;
+            int cx = 40, cy = 40;
+
+            Console.SetCursorPosition(ax, ay); Console.WriteLine("*");
+            Console.SetCursorPosition(bx, by); Console.WriteLine("*");
+            Console.SetCursorPosition(cx, cy); Console.WriteLine("*");
+        }
+        
+    }
+}
---
+++ lesson2_30.07.21/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace lesson2_30._07._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ex1.ex1();
+
+
+
+
+
+
+
+
+
+
+            //int sum = 0;
+            //for(int i = 10; i<=100;i++)
+            //{
+            //    if ((i % 100) / 10 + i % 10 == 10)
+            //    {
+            //        Console.WriteLine(i);
+            //        sum += i;
+            //    }
+            //}
+
+
+            //    int a = 12;
+            //    int b = 86;
+            //    int max;
+            //    int result;
+
+            //    max = (a > b) ? a : b;
+            //    result = NOD(a, b);
+            //    Console.WriteLine(a);
+            //    Console.WriteLine(b);
+
+            //}
+
+            //private static int NOD(int a, int b)
+            //{
+            //    while (a != b)
+            //    {
+            //        if (a > b)
+            //        {
+            //            a = a - b;
+            //        }
+            //        else
+            //        {
+            //            b -= a;
+            //        }
+            //    }
+            //    return a;
+            //}
+        }
+    }
+}
