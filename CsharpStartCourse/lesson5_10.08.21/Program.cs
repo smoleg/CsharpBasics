dc5e74db5d18420c297792505d991cdb3fba9152
Egor Smolyakov <smolyakov.en@gmail.com>
2021-08-11 12:33:24 +0300

    Lesson5

---
+++ CsharpStartCourse/lesson5_10.08.21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace lesson5_10._08._21
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] urls =
+            {
+                @"https://vk.com",
+                @"https://ya.ru",
+                @"https://google.com"
+            };
+
+            HttpClient client = new HttpClient();
+
+            foreach (var url in urls)
+            {
+                var req = client.GetStringAsync(url).Result;
+
+                int posStart = req.IndexOf("<title>") + "<title>".Length;
+                int posEnd = req.IndexOf("</title>");
+                int lenght = posEnd - posStart;
+
+                var res = req.Substring(posStart, lenght);
+                Console.WriteLine(res);
+            }
+            Console.ReadKey();
+        }
+    }
+}
