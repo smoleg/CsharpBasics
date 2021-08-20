87aba6a20371e0a684c3ce16cba085424c7963a6
Egor Smolyakov <smolyakov.en@gmail.com>
2021-08-20 15:13:54 +0300

    HW6

--- CsharpStartCourse/Exersices_HW6/Program.cs
+++ CsharpStartCourse/Exersices_HW6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exersices_HW6
 {
@@ -6,7 +7,28 @@ namespace Exersices_HW6
     {
         static void Main()
         {
+            string path = "studs.csv";
+            int n = 100;
 
+            // Формируем файл со списком из 100 студентов
+            StudentList students = new();
+            students.FileStudent(path, n);
+
+            // Заполняем коллекцию студентов
+            List<Student> studentList = new();
+            studentList = students.GetList(path);
+
+            // Задание а
+            students.CountCourse(studentList);
+            Console.WriteLine();
+
+            // Задание б
+            students.CountCourseAge(studentList);
+            Console.WriteLine();
+
+            // Задание в + г*
+            students.SortAge(studentList);
+            students.Print(studentList);
         }
     }
 }
